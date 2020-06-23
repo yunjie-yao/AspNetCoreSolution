64f37895f237716b79e33551c109ea292f59628d
yunjie.yao <932305768@qq.com>
2020-06-23 17:50:38 +0800

    NLog to ELK

--- YangXuAPI/Program.cs
+++ YangXuAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using Serilog;
 using Serilog.Events;
 using System;
@@ -38,6 +39,6 @@ namespace YangXuAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).UseNLog();
     }
 }
